2b7878b15918eaa72e334533e4c4645e4f90eb4c
oiBsa <b.azeem@alerton.com.au>
2022-12-06 10:57:20 +0500

    changes for vercel

--- barangaroo/settings.py
+++ barangaroo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-05h3xkqeyk=8r!t+-@wuf-spscypklu9g0vvvkclr+&ak*#76f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'barangaroo.herokuapp.com', 'barangaroo.herokuapp.com', ".herokuapp.com", "*"]
 X_FRAME_OPTIONS = 'ALLOWALL'
@@ -124,6 +124,8 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
