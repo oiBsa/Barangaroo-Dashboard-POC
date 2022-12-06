f9f4116c374feb4f8feae2dae79dc086905f72ae
oiBsa <b.azeem@alerton.com.au>
2022-12-06 11:15:23 +0500

    Vercel

--- barangaroo/settings.py
+++ barangaroo/settings.py
@@ -125,7 +125,6 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
